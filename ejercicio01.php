b8c4c64877cae41d659abc55d5013a5549e970fd
Ángeles <abuzon+git@posteo.es>
2025-10-20 12:36:09 +0200

    Termino ej1

--- ejercicio01.php
+++ ejercicio01.php
@@ -3,12 +3,9 @@
 # Crea una función que obtenga el número máximo de un array de números.
 
 $numbers = [];
-$numbersStr = "";
 $n;
-$maxNumber;
 $isPrompting = true;
 
-// Input
 do {
     $n = readline("Introduce un número cualquiera (o 0 para parar el bucle): ");
 
@@ -23,18 +20,18 @@ do {
 
 } while ($isPrompting);
 
-// Lógica
-$maxNumber = $numbers[0];
+echo "El numero máximo es " . findMaxNumber($numbers) . "\n";
 
-for ($i = 1; $i < count($numbers); $i++) {
-    if ($numbers[$i] > $maxNumber) {
-        $maxNumber = $numbers[$i];
+function findMaxNumber($array) {
+    $maxNumber = $array[0];
+
+    for ($i = 1; $i < count($array); $i++) {
+        if ($array[$i] > $maxNumber) {
+            $maxNumber = $array[$i];
+        }
     }
 
-    $numbersStr .= $numbers[$i] . " ";
+    return $maxNumber;
 }
-
-// Print
-echo "\nNúmeros: " . $numbersStr . "\nMáximo: " . $maxNumber . "\n";
 
 ?>
