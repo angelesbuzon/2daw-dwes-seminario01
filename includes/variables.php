b14cb4d1aa1b33f58033c8121eb399c014c7b40d
Ángeles <abuzon+git@posteo.es>
2025-10-22 10:37:06 +0200

    Errata

--- includes/variables.php
+++ includes/variables.php
@@ -1,7 +1,7 @@
 <?php
 
 #
-# Funciones que se usan varias veces en el proyecto
+# Variables que se usan varias veces en el proyecto
 #
 
 $inputMismatchNumber = "ERROR: Solo puedes introducir números (usa punto en vez de coma para decimales).\n";
