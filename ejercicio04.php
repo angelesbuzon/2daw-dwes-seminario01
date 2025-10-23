4c9d31067e67582fd72fe6d7803fb78966b0f06a
Ángeles <abuzon+git@posteo.es>
2025-10-23 09:40:36 +0200

    Termino ej4

--- ejercicio04.php
+++ ejercicio04.php
@@ -5,9 +5,6 @@
 # Un palíndromo es una palabra o frase que se lee igual de izquierda a derecha
 # que de derecha a izquierda ("ana", "reconocer", "anilina").
 
-include './includes/funciones.php';
-include './includes/variables.php';
-
 $word = "";
 
 do {
@@ -27,24 +24,26 @@ if (isPalindrome($word)) {
 # ---------------------
 
 function isPalindrome($word) {
-    # $left, $right;
-
     $slicingPoint = (int)(strlen($word) / 2);
-    var_dump($slicingPoint);
+    #var_dump($slicingPoint);
 
     # https://www.php.net/manual/en/function.substr.php
     $left = substr($word, 0, $slicingPoint);
-    var_dump($left);
     $right = substr($word, $slicingPoint+1, strlen($word));
-    var_dump($right);
+
+    # var_dump($left);
+    # var_dump($right);
 
     $isPalindrome = true;
+    $j = strlen($right)-1;
 
     for ($i = 0; $i < strlen($left) && $isPalindrome; $i++) {
-        for ($j = strlen($right)-1; $j >= 0 && $isPalindrome; $j++) {
-            if ($left[$i] !== $right[$j]) {
-                $isPalindrome = false;
-            }
+        # echo $left[$i] . ", " . $right[$j] . "\n";
+
+        if ($left[$i] !== $right[$j]) {
+            $isPalindrome = false;
+        } else {
+            $j--;
         }
     }
 
