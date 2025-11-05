225de79f1d07750550fa9e9807dabf9426cc9dcd
Ángeles <abuzon+git@posteo.es>
2025-11-05 09:11:11 +0100

    Añado ej19

--- ejercicio19.php
+++ ejercicio19.php
@@ -12,21 +12,22 @@ var_dump($text);
 
 $textWithoutVowels = removeVowels($text);
 
-echo $textWithoutVowels;
+echo $textWithoutVowels . "\n";
 
 function removeVowels(string $text): string {
-    foreach ($text as $char) {
-        /*
-        si este char pasado a minus es una vocal,
-        eliminar y recomponer el array
+    # str_replace() elimina todas las ocurrencias de lo que le indiques
+    # Sin cambiar el texto, podría hacer varios str_replace a palo seco (5 mayus, 5 minus),
+    # pero prefiero hacer un bucle que controle cuándo eliminar
 
-        o bien no un foreach sino un map?
-        */
-
+    for ($i = 0; $i < strlen($text); $i++) {
+        # https://inthedigital.co.uk/use-phps-in_array-to-compare-a-variable-to-multiple-values/
+        if (in_array(strtolower($text[$i]), ["a", "e", "i", "o", "u"])) {
+            $text = str_replace($text[$i], "", $text); # Eliminar el carácter actual por nada en $text
+            $i--; # Compensar eliminación por si hay vocales seguidas (str_replace)
+        }
     }
 
     return $text;
 }
-
 
 ?>
