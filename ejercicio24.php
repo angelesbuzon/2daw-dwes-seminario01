34fe52e479332b7aea7d2fc5d7a2f8caa3a4f1fd
Ángeles <abuzon+git@posteo.es>
2025-11-06 08:57:56 +0100

    Empiezo ej24

---
+++ ejercicio24.php
@@ -0,0 +1,21 @@
+<?php
+
+# WIP Ejercicio 24. Calculadora de descuentos con constantes
+# Crea un programa que utilice constantes para definir diferentes tipos de descuentos
+# (DESCUENTO_ESTUDIANTE, DESCUENTO_JUBILADO, DESCUENTO_VIP) 
+# y una función que calcule el precio final de un producto aplicando el descuento correspondiente según el tipo de cliente.
+
+include './includes/funciones.php';
+
+$STUDENT_DISCOUNT = 0.15;
+$RETIRED_DISCOUNT = 0.2;
+$VIP_DISCOUNT = 0.25;
+
+
+
+
+function calcFinalPrice(int $basePrice, string $typeOfDiscount): float {
+
+}
+
+?>
